4983c32a18413305ffd0a96daa47825da59c42e4
Akash-Salvi <akashnov100@gmail.com>
2021-09-29 23:02:12 +0530

    :fire:Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <div>
       Heloooo
-      <Child name="first" age={14} />
-      <Child name="last" age={15} />
-      <Child name="last" age={16} />
-      <Child name="first" age={17} />
+      <Child name="first" age={14} email="a.gmail.com" />
+      <Child name="last" age={15} email="b.gmail.com" />
+      <Child name="last" age={16} email="c.gmail.com" />
+      <Child name="first" age={17} email="d.gmail.com" />
     </div>
   );
 };
--- src/Child.tsx
+++ src/Child.tsx
@@ -2,10 +2,11 @@ type name_choice = 'first' | 'last';
 interface Props {
   name: name_choice;
   age: number;
+  email: string;
 }
 
-const Child = ({ name, age }: Props) => {
-  return <div>Child: {name}</div>;
+const Child = ({ name, age, email }: Props) => {
+  return <div>Child: {name + ' - ' + age + ' - ' + email}</div>;
 };
 
 export default Child;
