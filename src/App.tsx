6895ff40d065b220f352fb7c10daf9b264aac204
kothariji <kotharidhruv25@gmail.com>
2021-09-29 17:38:42 +0530

    cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Child from './Child';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const handleBtnClick = () => {
-    setCounter(counter + 1);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>{counter}</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" onClick={handleBtnClick}>
-          Increment
-        </button>
-      </header>
+    <div>
+      Heloooo
+      <Child name="first" age={14} />
+      <Child name="last" age={15} />
+      <Child name="last" age={16} />
+      <Child name="first" age={17} />
     </div>
   );
 };
